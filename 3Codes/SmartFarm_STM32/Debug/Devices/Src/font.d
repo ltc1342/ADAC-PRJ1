67636edec1bf3174039d3d7508c1acada70106df
ltc1342 <letienchien@gmail.com>
2026-06-26 12:25:37 +0700

    Ignore build artifacts

--- 3Codes/SmartFarm_STM32/Debug/Devices/Src/font.d
+++
@@ -1,5 +0,0 @@
-Devices/Src/font.o: ../Devices/Src/font.c \
- /home/ltc1342/stm32/ADAC-PRJ1/3Codes/SmartFarm_STM32/Devices/Inc/fonts.h \
- /home/ltc1342/stm32/ADAC-PRJ1/3Codes/SmartFarm_STM32/Common/Inc/app_types.h
-/home/ltc1342/stm32/ADAC-PRJ1/3Codes/SmartFarm_STM32/Devices/Inc/fonts.h:
-/home/ltc1342/stm32/ADAC-PRJ1/3Codes/SmartFarm_STM32/Common/Inc/app_types.h:
